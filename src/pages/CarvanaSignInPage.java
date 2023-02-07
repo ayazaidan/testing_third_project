f045a853f8efed4fa3ad1f50b049931e0e1603dc
ayazaidan <aya.zaidan.2015@gmail.com>
2023-02-07 18:47:05 +0400

    .

---
+++ src/pages/CarvanaSignInPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.FindBy;
+
+
+public class CarvanaSignInPage extends CarvanaBasePage {
+
+    public CarvanaSignInModalPage(){
+        super();
+    }
+
+    @FindBy(css = "input[data-qa='sign-in-email']")
+    public WebElement emailInput;
+
+    @FindBy(css = "button[data-qa='enter-email-submit']")
+    public WebElement continueButton;
+
+    @FindBy(css = "input[data-qa='sign-in-password']")
+    public WebElement passwordInput;
+
+    @FindBy(css = "button[data-qa='sign-in-submit']")
+    public WebElement signInButton;
+
+    @FindBy(css = "div[class*='error-banner__c']")
+    public WebElement errorMassage;
+
+
+
+}
+Footer
+        © 2023 GitHub, Inc.
+        Footer navigation
+        Terms
+        Privacy
+        Security
+        Status
+        Docs
+        Contact GitHub
+        Pricing
+        API
+        Training
+        Blog
+        About
